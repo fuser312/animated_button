b1f253dfc00e9275370483dde323d6ecbeec99cc
Sahil Singh <sahil.dogstar@gmail.com>
2019-10-22 11:08:55 +0530

    Tween and Scale Transition

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+//
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -19,23 +21,43 @@ class AnimatedButton extends StatefulWidget {
   _AnimatedButtonState createState() => _AnimatedButtonState();
 }
 
-class _AnimatedButtonState extends State<AnimatedButton> {
+class _AnimatedButtonState extends State<AnimatedButton> with SingleTickerProviderStateMixin{
+  AnimationController controller;
+  Animation<double> animation;
 
 bool isVisible = true;
   @override
+  void initState(){
+    controller = AnimationController(
+      duration: Duration(milliseconds : 800),
+      vsync: this
+    );
+    animation = CurvedAnimation(parent: controller, curve: Curves.bounceInOut);
+    controller.addStatusListener((AnimationStatus status){
+      if(status == AnimationStatus.completed){
+        controller.reverse();
+      }
+      else if(status == AnimationStatus.completed){
+        controller.forward();
+      }
+      print ("Status is now $status");
+    });
+    super.initState();
+  }
+  @override
   Widget build(BuildContext context) {
+    CurvedAnimation smmothAnimation = CurvedAnimation(parent: controller, curve: Curves.decelerate)
     return GestureDetector(
-      onTap: toggleVisible,
+      onTap: controller.forward,
       child: Container(
           width: 150,
           height: 150,
           color: Colors.grey.shade900,
-        child: AnimatedOpacity(
-          duration: Duration(milliseconds:500),
-          opacity: isVisible ? 1 : 0,
+        child: ScaleTransition(
+          scale: Tween(begin: 1.0, end: 3.0).animate(controller),
           child: Icon(
-            Icons.location_on,
-            color: Colors.white,
+            Icons.favorite,
+            color: Colors.red,
             size: 60,
           ),
         ),
